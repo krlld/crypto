3630450348246b6d623b7935e632176047917529
Kirill Dikun <kirilldikun25@gmail.com>
2024-10-13 21:08:06 +0300

    feat: unique constraint for favorite reports

--- data-analyze-service/src/main/resources/db/migration/V2__create_favorite_reports_table.sql
+++ data-analyze-service/src/main/resources/db/migration/V2__create_favorite_reports_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE data_analyze_service_favorite_reports
     id        BIGSERIAL PRIMARY KEY,
     user_id   BIGINT NOT NULL,
     report_id BIGINT NOT NULL,
+    UNIQUE (user_id, report_id),
     FOREIGN KEY (report_id) REFERENCES data_analyze_service_reports (id)
 );
