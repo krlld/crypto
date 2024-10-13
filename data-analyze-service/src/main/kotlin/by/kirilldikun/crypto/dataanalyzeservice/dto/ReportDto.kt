08535bdac25394560370a2928af8a5a2d28a4136
Kirill Dikun <kirilldikun25@gmail.com>
2024-10-13 19:19:57 +0300

    feat: unused import

--- data-analyze-service/src/main/kotlin/by/kirilldikun/crypto/dataanalyzeservice/dto/ReportDto.kt
+++ data-analyze-service/src/main/kotlin/by/kirilldikun/crypto/dataanalyzeservice/dto/ReportDto.kt
@@ -1,7 +1,6 @@
 package by.kirilldikun.crypto.dataanalyzeservice.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
