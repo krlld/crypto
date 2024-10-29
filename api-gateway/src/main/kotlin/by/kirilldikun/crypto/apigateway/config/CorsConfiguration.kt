e03ae83e8c5253a73ea0bee729742aab62fb5ce1
Kirill Dikun <kirilldikun25@gmail.com>
2024-10-29 18:36:00 +0300

    feat: cors config in gateway

--- api-gateway/src/main/kotlin/by/kirilldikun/crypto/apigateway/config/CorsConfiguration.kt
+++ api-gateway/src/main/kotlin/by/kirilldikun/crypto/apigateway/config/CorsConfiguration.kt
@@ -1,4 +1,4 @@
-package by.kirilldikun.crypto.commons.config
+package by.kirilldikun.crypto.apigateway.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
